767f21a14c57f4af8462420de11e312caf9fce82
Mehovic99 <anesmehovic28@gmail.com>
2023-09-28 21:50:29 +0200

    Created index.php and initialized HTML
    Created a file called index.php and inside of it initialized the head and the body part of the HTML so that I may display my PHP code in frontend

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- Initialize HTML on the site -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1.0">
+        <title>PHP Chat Project - Login</title>
+    </head>
+<body>
+
+</body>
+</html>
